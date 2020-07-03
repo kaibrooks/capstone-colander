409ce816cd05ae87106f6fceee4028b7ff8aacb0
Kai Brooks <kaibrooks@gmail.com>
2020-07-03 16:56:36 -0700

    Added 'Assignment' outputs to the CSV, added test data sources

--- src/load_csv.py
+++ src/load_csv.py
@@ -2,12 +2,13 @@
 # loads a csv and reads it (imagine that)
 
 import pandas as pd # loads csv
+import random # make a random assignment for demoing
 
 print("*** RUNNING LOAD_CSV.PY ***\n")
 
 ## load CSV
-df = pd.read_csv ('/io/students.csv') # load a file as a variable
-print('CSV data')
+df = pd.read_csv('/io/students.csv') # load a file as a variable
+print('Input CSV data')
 print(df) # output it
 
 # examples of data analysis
@@ -18,11 +19,19 @@ print("Average GPA    : ", df['GPA'].mean())
 # copy the first file so we don't overwrite it
 df.to_csv('/io/students.csv', index=False)
 
-# add project assignments
-new_data = pd.DataFrame({'Assignment': ['Superman NanoCape', 'Bee Swarm Laser', 'Gravity Inverter', 'Moon Tractor Beam']}) # project assignments
+## 'assign' a random project
+# make an array of random numbers
+project_assignment = []
+for i in range(0, len(df)):
+    n = random.randint(1,5) # assign random group
+    project_assignment.append(n)
+
+# add that column to the dataframe
+df['Assignment'] = '' # if no 'Assignment' column, create it
+new_data = pd.DataFrame({'Assignment': project_assignment}) # project assignments
 df.update(new_data) # add them to the csv
 
-## write new csv
+## write a new csv
 df.to_csv('/io/new_students.csv', index=False)
-print('CSV data with assignments')
+print('\nNew CSV data with assignments')
 print(df) # output it
--- src/main.py
+++ src/main.py
@@ -10,6 +10,6 @@ from datetime import date, datetime
 # startup information
 now = datetime.today().strftime('%Y-%m-%d %H:%M:%S') # get the date/time
 print("Container built", now) # print it
-
+ 
 # call another script
 os.system('python src/load_csv.py') # put what file actually want run here
