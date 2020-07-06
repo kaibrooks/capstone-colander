af291fc016ac05f6307a4978689ba176df466c77
Kai Brooks <kaibrooks@gmail.com>
2020-07-05 20:33:50 -0700

    Start of scoring function

--- src/main.py
+++ src/main.py
@@ -12,4 +12,4 @@ now = datetime.today().strftime('%Y-%m-%d %H:%M:%S') # get the date/time
 print("Container built", now) # print it
  
 # call another script
-os.system('python src/load_csv.py') # put what file actually want run here
+os.system('python src/test/test_score.py') # put what file we actually want run here
