1818885c019756643082571e1a760a3c403f208d
David Yakovlev <Yakovlev.david@gmail.com>
2020-07-13 22:41:09 -0700

    Update main.py
    command line parser

--- src/main.py
+++ src/main.py
@@ -4,12 +4,66 @@
 # it's here because docker always builds "main.py"
 # don't develop in this
 
+import sys
+import argparse
 import os
 from datetime import date, datetime
+
 
 # startup information
 now = datetime.today().strftime('%Y-%m-%d %H:%M:%S') # get the date/time
 print("Container built", now) # print it
- 
+
+# default csv file names & program mode
+studentsFile = 'io/students.csv'
+projectsFile = 'io/projects.csv'
+settingsFile = 'io/settings.csv'
+outputFile   = 'io/assignment.csv'
+programMode  = 'Assignment'
+
+# accepted command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--students", help = "Students CSV filename", required = False)
+parser.add_argument("-p", "--projects", help = "Projects CSV filename", required = False)
+parser.add_argument("-u", "--settings", help = "User Settings CSV filename", required = False)
+parser.add_argument("-o", "--output", help = "Output (assignment) CSV filename", required = False)
+parser.add_argument("-a", "--assign", help = "Run the program in Assignment mode", required = False, action='store_true')
+parser.add_argument("-c", "--score", help = "Run the program in Scoring mode", required = False, action='store_true')
+
+argument = parser.parse_args()
+
+# assign any user provided csv file names and mode preference
+if argument.students:
+    studentsFile = argument.students
+if argument.projects:
+    projectsFile = argument.projects
+if argument.settings:
+    settingsFile = argument.settings
+if argument.output:
+    outputFile = argument.output
+if argument.score:
+    programMode = 'Scoring'
+if argument.assign:
+    programMode = 'Assignment'
+
+# for debugging, remove when no longer necessary
+print("Students csv is: {0}".format(studentsFile))
+print("Projects csv is: {0}".format(projectsFile))
+print("Settings csv is: {0}".format(settingsFile))
+print("Output csv is: {0}".format(outputFile))
+print("Program mode is: {0}".format(programMode))
+
+# verify user provided files exist and that they are .csv files
+userFiles = [studentsFile, projectsFile, settingsFile]
+for file in userFiles:
+    if not os.path.exists(file):
+        sys.exit("ERROR: {0} can not be found. Terminating Program.".format(file))
+    if not file.endswith('.csv'):
+        sys.exit("ERROR: {0} is not a '.csv' file. Input files must have .csv extension. Terminating Program.".format(file))
+
+# warn user if the output they provided already exists and that the output will be named differently than provided
+if os.path.exists(outputFile):
+    print("WARNING: Output file {0} already exists.".format(outputFile), outputFile.replace('.csv', '(2).csv will be created instead.'))
+
 # call another script
-os.system('python src/load_csv.py') # put what file actually want run here
+os.system('python src/run_ga.py') # put what file we actually want run here
