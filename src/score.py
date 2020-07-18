521b53ba6eb190405dc045d4cefdbfaeec466865
Jaeyoon Lee <56676415+fortniz@users.noreply.github.com>
2020-07-18 14:33:22 -0700

    initial coding on score module

--- src/score.py
+++ src/score.py
@@ -1,10 +1,5 @@
 # points calculation for capstone-colander
-# Kai Brooks
 # github.com/kaibrooks/capstone-colander
-
-# returns points_max, divided evenly down by num_projects, depending on which assignment_choice a student gets
-# eg, 5 projects, 100 points max => 20 points loss per 'step'
-# gets first pick = 100 points, second pick = 80 points, third pick = 60 points, etc
 
 import math # ceil()
 
@@ -20,3 +15,86 @@ def points( assignment_choice, num_projects, points_max ):
     else: 
         total = math.ceil( points_max - (points_max / num_projects)*(assignment_choice - 1) )
     return total
+
+##############################################################################################
+
+# three functions below are parts of the scoring module functions
+# tasked for Jaeyoon Lee
+# Scoring module shall require data structure from CSV input files
+# dataStruct Students {
+#   studentID
+#	studentChoice[N]
+#	studentGPA
+#   studentESL
+#   studentPriority
+#   studentAvoid
+# }
+#dataStruct Projects {
+#	projectID
+#	minTeamSize
+#	maxTeamSize
+#	teamActualSize
+# }
+#dataStruct Settings {
+#	teamSize
+#	lowGPAThreshold
+#	maxLowGPAStudents
+#	maxESLStudents
+#	maxRunTime
+#	weightMaxLowGPAStudents
+#	weightMaxESLStudents
+#	weightTeamSize
+#	weightStudentPriority
+#	weightStudentChoice1
+#	weightAvoid
+# }
+
+### variables passed when these functions get called
+# studentGPA: student GPA from students.csv [float]
+# lowGPAThreshold: minimum GPA cutoff from settings.csv [float]
+# maxLowGPAStudents: maximum number of students allowed in a group that fail GPA cutoff [int]
+# minTeamSize: minimum group size allowed from project.csv [int]
+# maxTeamSize: maximum group size allowed from project.csv [int]
+# teamSize: desired group size from settings.csv [int]
+# studentID: student identifier [int]
+# studentAvoid: student to avoid in a group [int]
+# weightMaxLowGPAStudents: score from settings.csv [int]
+# weightTeamSize: score from settings.csv [int]
+# weightAvoid: score from settings.csv [int]
+###
+
+
+# This calculates bonus points for having fewer students than maxLowGPAStudents in a group
+def pointMaxLowGPAStudents(studentGPA, lowGPAThreshold, maxLowGPAStudents, weightMaxLowGPAStudents)
+    # Look at assignments column
+    # Check studentGPA and check lowGPAThreshold
+    # Count student[i] that studentGPA < lowGPAThreshold
+    # if student[i] < maxLowGPAStudents
+        # then weightMaxLowGPAStudents += 50
+        # else weightMaxLowGPAStudents += 0
+    return weightMaxLowGPAStudents
+
+# This calculates bonus points for having met group size constraints
+def pointsTeamSize(minTeamSize, maxTeamSize, teamSize, weightTeamSize)
+    # Look at assignments column
+    # Count projectStudent[i] in each projectID
+    # if minTeamSize < projectStudent[i] < maxTeamSize
+        # then weightTeamsize += 50
+        # else weightTeamsize += 0
+    # if projectStudents[i] == teamSize
+        # then weightTeamSize += 50
+        # else weightTeamSize += 0
+    # else
+        # weightTeamSize += 0
+    return weightTeamSize
+
+# This calculates bonus for not violating student disallowance constraint
+def pointsAvoid(studentID, studentAvoid, weightAvoid)
+    # Look at assignments column
+    # Check studentAvoid for each studentID
+    # Check if studentAvoid == studentID within the same projectID
+    # if studentID == studentAvoid
+        # then weightAvoid += 0
+    # else if studentID != studentAvoid for any studentID in same projectID
+        # then weightAvoid += 100
+    return weightAvoid
