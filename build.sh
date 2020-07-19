e4a8a5325d73759303c1e5b7833c306a9cb33744
Kai Brooks <kaibrooks@gmail.com>
2020-07-18 18:18:22 -0700

    Adjusted Docker behavior

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 # build and run the docker container
 # Kai Brooks
 # github.com/kaibrooks
-#
+
 chmod +x build.sh # give execution privs to this script
 docker build -t capstone-colander . # build it
 
+# remove the old container
+# this stops the container from continuing in the background if re-run
+docker stop cc
+
 # docker run -it --entrypoint sh capstone-colander:latest   # build to explore the container
-docker run -it --rm -v `pwd`/io:/io capstone-colander:latest # gotta bind io if we want to output data
+docker run -it --name cc --rm -v `pwd`/io:/io capstone-colander:latest # gotta bind io if we want to output data
