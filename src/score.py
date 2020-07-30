c4cefd7190f8900425444f978bdcf429548eca49
Jaeyoon Lee <56676415+fortniz@users.noreply.github.com>
2020-07-30 06:05:59 -0700

    Update score.py

--- src/score.py
+++ src/score.py
@@ -1,5 +1,4 @@
 # Bliss Brass & Jae Lee
-#
 
 import math # ceil()
 import load_csv
@@ -55,103 +54,89 @@ scoringMode(cat)
 
 ##############################################################################################
 
-# three functions below are parts of the scoring module functions
-# tasked for Jaeyoon Lee
-# Scoring module shall require data structure from CSV input files
-# dataStruct Students {
-#   studentID
-#	studentChoice[N]
-#	studentGPA
-#   studentESL
-#   studentPriority
-#   studentAvoid
-# }
-#dataStruct Projects {
-#	projectID
-#	minTeamSize
-#	maxTeamSize
-#	teamActualSize
-# }
-#dataStruct Settings {
-#	teamSize
-#	lowGPAThreshold
-#	maxLowGPAStudents
-#	maxESLStudents
-#	maxRunTime
-#	weightMaxLowGPAStudents
-#	weightMaxESLStudents
-#	weightTeamSize
-#	weightStudentPriority
-#	weightStudentChoice1
-#	weightAvoid
-# }
-
-### variables passed when these functions get called
-# studentGPA: student GPA from students.csv [float]
-# lowGPAThreshold: minimum GPA cutoff from settings.csv [float]
-# maxLowGPAStudents: maximum number of students allowed in a group that fail GPA cutoff [int]
-# minTeamSize: minimum group size allowed from project.csv [int]
-# maxTeamSize: maximum group size allowed from project.csv [int]
-# teamSize: desired group size from settings.csv [int]
-# studentID: student identifier [int]
-# studentAvoid: student to avoid in a group [int]
-# weightMaxLowGPAStudents: score from settings.csv [int]
-# weightTeamSize: score from settings.csv [int]
-# weightAvoid: score from settings.csv [int]
-###
-
-
-### sample variables and values
-#studentID = [1,2,3,4,5,6,7,8,9,10]  # students from 1 to 10
-#studentGPA = [3.00,2.99,2.88,2.77,2.66,2.55,2.44,2.33,2.22,2.11]    # student GPAs for 1 to 10
-#studentESL = [0,0,1,0,1,0,0,0,1,0]  # students 3, 5, and 9 are ESL students
-#studentESL = [3,5,9]    # students 3, 5, and 9 are ESL students
-#studentChoice = [1,2,3,1,2,3,1,2,3,1] # student project choice
-#studentPriority = [0,0,0,0,1,0,0,0,0,1] # student priority flag
-#studentPriority = [5,10] # student priority flag
-#studentAvoid = [,3,2,,,,,,3,9] # student avoid choice
-#projectID = [1,2,3] # project numbers
-#minTeamSize = [2,2,3] # min group size for each project
-#maxTeamSize = [4,4,5] # max grou psize for each project
-#teamSize = [4,3,3] # group size desired by user
-#lowGPAThreshold = [2.40] # min GPA cutoff
-#maxLowGPAStudents = [2] # min GPA cutoff students limit
-#maxESLStudents = [2] # ESL student limit
-#maxRunTime = [30] # program max run time
-#studentAssignments = [1,1,2,2,3,3,1,2,1,2]
+# sample local variables and values
+studentID = [1,2,3,4,5,6,7,8,9,10]  # students from 1 to 10
+studentGPA = [3.00,2.99,2.88,2.77,2.66,2.55,2.44,2.33,2.22,2.11]    # student GPAs for 1 to 10
+studentESL1 = [0,0,1,0,1,0,0,0,1,0]  # students 3, 5, and 9 are ESL students one way to read it
+studentESL2 = [3,5,9]    # students 3, 5, and 9 are ESL students the other way to read it
+studentChoice = [1,2,3,1,2,3,1,2,3,1] # student project choice
+studentPriority1 = [0,0,0,0,1,0,0,0,0,1] # student priority flag one way to read it
+studentPriority2 = [5,10] # student priority flag the other way to read it
+studentAvoid = ['blank',3,2,'blank','blank','blank','blank','blank',3,9] # student avoid choice
+projectID = [1,2,3] # project numbers 1 to 3
+minTeamSize = [2,2,3] # min group size for each project for projects 1 to 3
+maxTeamSize = [4,4,5] # max grou psize for each project for projects 1 to 3
+min_teamSize = [2,2,2] # min group size desired by user for projects 1 to 3
+max_teamSize = [4,4,5] # max group size dsired by user for projects 1 to 3
+lowGPAThreshold = 2.40 # min GPA cutoff
+maxLowGPAStudents = 2 # min GPA cutoff students limit is 2 students
+maxESLStudents = 2 # ESL student limit is 2 students
+maxRunTime = 30 # program max run time is 30 min (not important)
+studentAssignments = [1,1,2,2,3,3,1,2,1,2] # GA assignments for student 1 to 10
+group_size = [4,4,2]
+weightMaxLowGPAStudents = 100
+weightMaxESLStudents    = 75
+weightTeamSize          = 50
+weightStudentPriority   = 100
+weightStudentChoice1    = 75
+weightAvoid             = 60
 
 # This calculates bonus points for having fewer students than maxLowGPAStudents in a group
-#def pointMaxLowGPAStudents(studentGPA, lowGPAThreshold, maxLowGPAStudents, weightMaxLowGPAStudents)
+def pointMaxLowGPAStudents():
+    points = 0
     # Look at assignments column
     # Check studentGPA and check lowGPAThreshold
     # Count student[i] that studentGPA < lowGPAThreshold
     # if student[i] < maxLowGPAStudents
+    print(lowGPAThreshold, 'min GPA cutoff')
+    print(maxLowGPAStudents, 'max students with min GPA in a group')
+    for studentID[i] in score:
+        if studentGPA[float] < lowGPAThreshold[float]:
+            if studentID[i] < maxLowGPAStudents
+                points += weightMaxLowGPAStudents
+                print(points, 'total score =')
+            else:
+                pass
         # then weightMaxLowGPAStudents += 50
         # else weightMaxLowGPAStudents += 0
-    #return weightMaxLowGPAStudents
+    return points
 
 # This calculates bonus points for having met group size constraints
-#def pointsTeamSize(minTeamSize, maxTeamSize, teamSize, weightTeamSize)
+def pointsTeamSize():
     # Look at assignments column
     # Count projectStudent[i] in each projectID
-    #for 
     # if minTeamSize < projectStudent[i] < maxTeamSize
+    points = 0
+    for i in score:
+    print(minTeamSize[i], 'minTeamSize')
+    print(maxTeamSize[i], 'maxTeamSize')
+    print(group_Size[i], 'group size')
+        if minTeamsize[i] < group_size[i] < maxTeamSize[i]:
         # then weightTeamsize += 50
         # else weightTeamsize += 0
-    # if projectStudents[i] == teamSize
-        # then weightTeamSize += 50
-        # else weightTeamSize += 0
-    # else
-        # weightTeamSize += 0
-    #return weightTeamSize
+            points += weightTeamSize
+            print(points, 'total points =')
+        # if a project does not have min and max TeamSize values, then default to settings.csv teamSize
+        else min_temaSize[i] < group_size[i] < max_teamSize[i]:
+             points += weightTeamSize
+             print(points, 'total points =')
+    return points
 
 # This calculates bonus for not violating student disallowance constraint
-#def pointsAvoid(studentID, studentAvoid, weightAvoid)
+def pointsAvoid():
     # Look at assignments column
     # Check studentAvoid for each studentID
     # Check if studentAvoid == studentID within the same projectID
     # if studentID == studentAvoid
         # then weightAvoid += 0
+    points = 0
+    for i in studentAssignments:
+    if studnetID[i] == studnetAvoid[i]
+        points += 0
+        print(points, 'total points =')
     # else if studentID != studentAvoid for any studentID in same projectID
         # then weightAvoid += 100
-    #return weightAvoid
+    else:
+        points += weightAvoid
+        print(points, 'total points =')
+    return points
