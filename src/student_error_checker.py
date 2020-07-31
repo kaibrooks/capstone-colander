83915f61f0436f7db36bb86bbf3803657ad70305
Zoe DeFreitas <zcd2@pdx.edu>
2020-07-30 17:02:22 -0700

    Store student data as global series (not including Avoid or Choice data)

--- src/student_error_checker.py
+++ src/student_error_checker.py
@@ -1,38 +1,90 @@
-# load students csv file
-studentsFileData = pd.read_csv(studentsFile)
+import sys
+import pandas as pd
 
-# verify all column headers are present in projects csv file
-## NEEDS to be updated to send warnings for missing studentPriority and studentAvoid
-studentsColumns = ['studentID', 'studentChoice1', 'studentGPA']
-for col in studentsColumns:
-    if col not in studentsFileData.columns:
-        sys.exit("ERROR: Required {0} column header not found in the projects csv file. Terminating Program.".format(col))
+def studentsHandler(studentsFile):
 
-# debug print to verify data has been loaded
-print(studentsFileData)
+    global studentID
+    global studentGPA
+    global studentESL
+    global studentPriority
+    #global studentChoiceN
+    #global studentAvoidN
 
-#Create a series to indicate datatypes of each column
-studentDTypes = studentsFileData.dtype
+    #flags to indicate that optional headers have been included default False
+    #colPriority = False
+    #colAvoid = False
 
-#debug print to verify datatype series is correct
-print(studentDTypes)
+    #load students csv file
+    studentsFileData = pd.read_csv(studentsFile)
 
-#Validate integers are present in studentID
-if studentDTypes['studentID'] != 'int64'
-    sys.exit("ERROR: Unexpected data type found in studentID. Terminating Program.")
+    # debug print to verify data has been loaded
+    print(studentsFileData)
+
+    ######Store Data#######
+    studentID = studentsFileData['studentID'].copy()
+    studentGPA = studentsFileData['studentGPA'].copy()
+    studentESL = studentsFileData['studentESL'].copy()
+    studentPriority = studentsFileData['studentPriority'].copy()
+
+    #debug print to verify studentID has been stored
+    print("Student ID Array Test")
+    print(studentID)
+
+    
 
 
-# if values for team sizes are blank, enter size from settings.csv and then verify all values are integers
-## REVIEW (maybe use for esl/priority flags?)
-#projectsFileData['minTeamSize'] = projectsFileData['minTeamSize'].fillna(42)  # 42 needs to be changed to settings.csv value
-#minTeamSize = int_checker('minTeamSize')
-#projectsFileData['maxTeamSize'] = projectsFileData['maxTeamSize'].fillna(42)
-#maxTeamSize = int_checker('maxTeamSize')
+    ########################Validate Data######################
+    #########################UNFINISHED########################
+    # verify all column headers are present in students csv file
+    #studentsColumns = ['studentID', 'studentChoice1', 'studentGPA']
+    #for col in studentsColumns:
+    #    if col not in studentsFileData.columns:
+    #        sys.exit("ERROR: Required {0} column header not found in the students csv file. Terminating Program.".format(col))
+
+    # look for optional column headers in students csv file
+    #if 'studentPriority' not in studentsFileData.columns:
+    #    print("Warning: studentPriority column header not found in the students csv file.")
+    #else:
+        #set colPriority flag to true
+    #    colPriority = True
+        #debug print
+    #    print("Priority Flag column found")
+
+    #if 'studentAvoid1' not in studentsFileData.columns:
+    #    print("Warning: studentAvoid1 column header not found in the students csv file. No avoids will be considered for the solution.")
+    #else:
+        #set colAvoid flag to true
+    #    colAvoid = True
+        #debug print
+    #    print("Avoid1 Flag column found")
+
+    
+
+    #Create a series to indicate datatypes of each column
+    #studentDTypes = studentsFileData['studentID'].dtype
+    #studentDTypes = studentsFileData.dtype
+
+    #debug print to verify datatype series is correct
+    #print(studentDTypes)
+
+    #Validate only integers are present in studentID
+    #if studentsFileData['studentID'].dtype != np.int64
+    #    print('studentID incorrect ')        
+    #else:
+    #    sys.exit("ERROR: Unexpected data type found in studentID. Terminating Program.")
+    #Validate only integers are present in studenChoice1
+    #if studentDTypes['studentChoice1'] != 'int64'
+    #    sys.exit("ERROR: Unexpected data type found in studentChoice1. Terminating Program.")
+    #Validate only floats are present in studentID
+    #if studentDTypes['studentGPA'] != 'float64'
+    #    sys.exit("ERROR: Unexpected data type found in studentGPA. Terminating Program.")
+
+    # if values for team sizes are blank, enter size from settings.csv and then verify all values are integers
+    ## REVIEW (maybe use for esl/priority flags?)
+    #projectsFileData['minTeamSize'] = projectsFileData['minTeamSize'].fillna(42)  # 42 needs to be changed to settings.csv value
+    #minTeamSize = int_checker('minTeamSize')
+    #projectsFileData['maxTeamSize'] = projectsFileData['maxTeamSize'].fillna(42)
+    #maxTeamSize = int_checker('maxTeamSize')
 
 
 
-# call another script
-if not programMode == 'verbose':
-    print('Calling the next function...')
-    #os.system('python src/test/test_main.py') # put what file we actually want run here
-#os.system('python src/run_ga.py') # put what file we actually want run here
