abb73a263e956405ae41a4369aeeb4e65a456d98
David Yakovlev <Yakovlev.david@gmail.com>
2020-07-31 21:38:38 -0700

    more errors handled

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ import argparse
 import os
 from datetime import datetime
 import load_csv
+import pandas as pd
 #import run_ga ...when GA is completed and tested
 #import scoring ...when Scoring is completed and tested
 
 # startup information
 now = datetime.today().strftime('%Y-%m-%d %H:%M:%S')  # get the date/time
-print("Container built", now)  # print it
+print("Program started", now)  # print it
 
 
 def main():
@@ -23,13 +24,12 @@ def main():
 
     # accepted command line arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("-s", "--students", help="Students CSV filename", required=False, default='students.csv')
-    parser.add_argument("-p", "--projects", help="Projects CSV filename", required=False, default='projects.csv')
-    parser.add_argument("-u", "--settings", help="User Settings CSV filename", required=False, default='settings.csv')
-    parser.add_argument("-o", "--output", help="Output (assignment) CSV filename", required=False, default='assignment.csv')
+    parser.add_argument("-s", "--students", help="Students CSV filename", required=False, default='io/students.csv')
+    parser.add_argument("-p", "--projects", help="Projects CSV filename", required=False, default='io/projects.csv')
+    parser.add_argument("-u", "--settings", help="User Settings CSV filename", required=False, default='io/settings.csv')
+    parser.add_argument("-o", "--output", help="Output (assignment) CSV filename", required=False, default='io/assignment.csv')
     parser.add_argument("-a", "--assign", help="Run the program in Assignment mode", required=False, action='store_true')
     parser.add_argument("-c", "--score", help="Run the program in Scoring mode", required=False, action='store_true')
-    parser.add_argument("-v", "--verbose", help="Run the program in verbose mode", required=False, action='store_true')
 
     argument = parser.parse_args()
 
@@ -46,41 +46,48 @@ def main():
         programMode = 'Scoring'
     if argument.assign:
         programMode = 'Assignment'
-    if argument.verbose:
-        programMode = 'Verbose'
 
-    # verify user provided files exist and that they are .csv files
-    userFiles = [studentsFile, projectsFile, settingsFile]
-    for file in userFiles:
-        if not os.path.exists(file):
-            if not file.endswith('.csv'):
-                sys.exit("ERROR: {0} is neither a '.csv' file nor can it be found in the directory. Input files must have .csv extension. Terminating Program.".format(file))
-            sys.exit("ERROR: {0} can not be found. Terminating Program.".format(file))
-        if not file.endswith('.csv'):
-            sys.exit("ERROR: {0} is not a '.csv' file. Input files must have .csv extension. Terminating Program.".format(file))
-
-    # inform user if the output they provided already exists and terminate program
-    if os.path.exists(outputFile) and programMode == 'Assignment':
+    # if output user provided already exists when running in Assignment mode, terminate program
+    if programMode == 'Assignment' and os.path.exists(outputFile):
         sys.exit("ERROR: {0} already exists in the directory. Enter a unique output file name using the -o FILENAME command. Terminating Program.".format(outputFile))
 
-    return projectsFile, studentsFile, settingsFile, programMode
+    # function to verify user provided files exist and that they are csv files
+    def csvFileCheck(csvFileName):
+        if not os.path.exists(csvFileName):
+            # if original filename not found, add .csv extension and check again
+            tempFileName = csvFileName + '.csv'
+            if not os.path.exists(tempFileName):
+                sys.exit("ERROR: {0} csv file can not be found. Terminating Program.".format(csvFileName))
+            csvFileName = tempFileName
+        try:
+            tempDataStruct = pd.read_csv(csvFileName)
+        except ValueError:
+            sys.exit("ERROR: {0} is not a valid csv file. Terminating Program.".format(csvFileName))
+        return tempDataStruct
+
+    # load projects csv file
+    settingsData = csvFileCheck(settingsFile)
+    projectsData = csvFileCheck(projectsFile)
+    studentsData = csvFileCheck(studentsFile)
+
+    return settingsData, projectsData, studentsData, programMode
 
 
 if __name__ == "__main__":
 
     # command line parser and error handling
-    projFile, studFile, settFile, progMode = main()
+    settingsFileData, projectsFileData, studentsFileData, progMode = main()
 
     # read, parse, and handle errors of all three csv files
-    load_csv.settingsHandler(settFile)
-    load_csv.projectsHandler(projFile)
-    # load_csv.studentsHandler(studFile) ....when complete, load student csv handler
+    load_csv.settingsHandler(settingsFileData)
+    load_csv.projectsHandler(projectsFileData)
+    #load_csv.studentsHandler(studentsFileData) ....when complete, call student csv handler
 
     if progMode == 'Assignment':
         print("\nProgram running in Assignment mode with a max run time of {0} minutes.".format(load_csv.maxRunTime))
-        # run_ga.geneticAlgorithmFunction() ...when complete, run GA for assignment mode
+        #run_ga.geneticAlgorithmFunction() ...when complete, call GA for assignment mode
     elif progMode == 'Scoring':
         print("\nProgram running in Scoring mode.")
-        # scoring.scoreFunction() ...when complete, run Scoring function
+        #scoring.scoreFunction() ...when complete, call Scoring function
 
     print("\n*** Program has completed running ***")
--- src/load_csv.py
+++ src/load_csv.py
@@ -5,13 +5,10 @@ import sys
 import pandas as pd
 
 
-def projectsHandler(projectsFile):
+def projectsHandler(projectsFileData):
     global minTeamSize
     global maxTeamSize
     global projectIDs
-
-    # load projects csv file
-    projectsFileData = pd.read_csv(projectsFile)
 
     # verify required project csv headers are present
     projectsColumns = ['projectID', 'minTeamSize', 'maxTeamSize']
@@ -19,18 +16,21 @@ def projectsHandler(projectsFile):
         if col not in projectsFileData.columns:
             sys.exit("ERROR: Required {0} column header not found in the projects csv file. Terminating Program.".format(col))
 
-    # function to verify that values listed in the projects csv are integers within required range
+    # function to verify that all require values in projects csv file are integers within the required range
     def int_checker_projects(columnName):
-        intArray = []
+
+        tempArray = []
         for value in projectsFileData[columnName]:
-            try:
-                intArray.append(int(value))
-            except ValueError:
+            if isinstance(value, float):
+                if value.is_integer():
+                    value = int(value)
+            if isinstance(value, int):
+                if (value < 1) or (value > pow(2, 64)):
+                    sys.exit("ERROR: Value {0} in the {1} column must be an integer greater than zero and less than 2^64.".format(value, columnName))
+                tempArray.append(value)
+            else:
                 sys.exit("ERROR: {0} in the {1} column is not an integer. Terminating Program.".format(value, columnName))
-        for value in intArray:
-            if (value < 0) or (value > pow(2, 64)):
-                sys.exit("ERROR: Value {0} in the {1} column must be an integer greater than zero and less than 2^64.".format(value, columnName))
-        return intArray
+        return tempArray
 
     # verify that all values in program csv file are integers
     projectIDs = int_checker_projects('projectID')
@@ -56,7 +56,7 @@ def projectsHandler(projectsFile):
         print("\nWARNING: gap found in projectID sequence in the projects csv file.")
 
 
-def settingsHandler(settingsFile):
+def settingsHandler(settingsFileData):
     global weightMaxLowGPAStudents
     global weightMaxESLStudents
     global weightTeamSize
@@ -70,10 +70,6 @@ def settingsHandler(settingsFile):
     global maxESLStudents
     global lowGPAThreshold
 
-    # load projects csv file
-    settingsFileData = pd.read_csv(settingsFile)
-
-    print(settingsFileData)
     # verify required settings csv headers are present
     settingsColumns = ['name', 'min', 'max', 'points']
     for col in settingsColumns:
@@ -87,15 +83,38 @@ def settingsHandler(settingsFile):
         if row not in settingsFileData['name'].values:
             sys.exit("ERROR: Required {0} row 'name' not found in the settings csv file. Terminating Program.".format(row))
 
-    # function to verify that values listed in the settings csv are integers within range
+    # function to verify values in the csv file are integers.
     def int_checker_settings(value, rowName, minValue):
-        try:
-            tempInt = int(value)
-        except ValueError:
-            sys.exit("ERROR: {0} in the {1} row is not an integer. Terminating Program.".format(value, rowName))
-        if (value < minValue) or (value > pow(2, 64)):
-            sys.exit("ERROR: Value {0} in the {1} column must be an integer greater than zero and less than 2^64.".format(value, rowName))
-        return tempInt
+
+        if isinstance(value, int):
+            if (value < minValue) or (value > pow(2, 64)):
+                 sys.exit("ERROR: Value {0} in the {1} column must be an integer greater than zero and less than 2^64.".format(value, rowName))
+            return value
+
+        # function to verify integer if data type of value is not originally set as int (dtype could be obj or float)
+        def is_integer_settings(value, rowName, minValue):
+            if value.is_integer():
+                tempInt = int(value)
+                if (tempInt < minValue) or (tempInt > pow(2, 64)):
+                    sys.exit("ERROR: Value {0} in the {1} column must be an integer greater than zero and less than 2^64.".format(value, rowName))
+                return tempInt
+            else:
+                sys.exit("ERROR: {0} in the {1} row is not an integer. Terminating Program.".format(value, rowName))
+
+        # if pandas set column data type to float
+        # this would happen when there are only floats or empty fields found in the column
+        if isinstance(value, float):
+            return is_integer_settings(value, rowName, minValue)
+
+        # if pandas set column data type to a python object
+        # this would happen when there are strings found in the column
+        if isinstance(value, object):
+            try:
+                tempInt = float(value)
+            except ValueError:
+                sys.exit("ERROR: {0} in the {1} row is not an integer. Terminating Program.".format(value, rowName))
+            return is_integer_settings(tempInt, rowName, minValue)
+
 
     # verify required values in 'points' column are integers
     # if they are, assign value to global variable for scoring function to use
@@ -125,13 +144,14 @@ def settingsHandler(settingsFile):
 
     # verify that provided maxRunTime is an integer.
     # If it is, set it as maxRunTime for the program, otherwise, use default hour run time.
-    maxRunTime = 60
     try:
         maxRunTime = int(settingsFileData.set_index('name').at['maxRunTime', 'max'])
     except:
-        pass
+        maxRunTime = 60
+        print("WARNING: maxRunTime value in settings csv is invalid. Running with default value of 60 minutes.")
     if maxRunTime < 0:
-        sys.exit("maxRunTime in the settings csv must be an integer between 1 and 2^64.")
+        maxRunTime = 60
+        print("WARNING: maxRunTime in the settings csv is not an integer between 1 and 2^64. Running with default value of 60 minutes.")
 
     # verify that provided lowGPAThreshold is not empty and that it's a float within range
     # If it is, assign value to global variable for scoring function to use.
