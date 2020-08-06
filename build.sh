20c8ada518f7270bbad4bf64fe234fd5b938882a
Kai Brooks <kaibrooks@gmail.com>
2020-08-06 10:08:37 -0700

    Added docker package information

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 # build and run the docker container
 # Kai Brooks
 # github.com/kaibrooks
+# use 'docker exec cc pip list' to list all the packages the container uses
 
 chmod +x build.sh # give execution privs to this script
 docker build -t capstone-colander . # build it
