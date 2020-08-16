8ba4968e6b81d35252e60faa5b38f44fc1250228
Jaeyoon Lee <56676415+fortniz@users.noreply.github.com>
2020-08-16 14:13:57 -0700

    score functions cleaned up

--- src/score.py
+++ src/score.py
@@ -57,7 +57,6 @@ def pointsStudentPriority(inputArray):
 
     return totalPSP
 
-
 # This calculates bonus points for having fewer students than maxLowGPAStudents in a group
 def pointsMaxLowGPAStudents(inputArray):
     totalPML = 0
@@ -66,10 +65,6 @@ def pointsMaxLowGPAStudents(inputArray):
     maxLow = load_csv.maxLowGPAStudents
     maxLow_group = [0] * len(load_csv.studentID)
     group_size = [0] * len(load_csv.studentID)
-
-    # print(group_size)
-    # print(minGPA, ': min GPA cutoff')
-    # print(maxLow, ': max students with min GPA in a group')
 
     def maxgroup(maxLow_group):
         for i in range(len(load_csv.studentID)):
@@ -85,9 +80,6 @@ def pointsMaxLowGPAStudents(inputArray):
     maxgroup(maxLow_group)
     sizegroup(group_size)
 
-    # print(sizegroup(group_size))
-    # print('Number of students < minGPA for each group: ', maxgroup(maxLow_group))
-
     for i in range(len(maxLow_group)):
         if maxLow_group[i] >= 0 and maxLow_group[i] <= maxLow and group_size[i] > 0:
             totalPML += weight_pml
@@ -98,93 +90,45 @@ def pointsMaxLowGPAStudents(inputArray):
 
     return totalPML
 
-
 # This calculates bonus points for having met group size constraints
 def pointsTeamSize(inputArray):
     totalPTS = 0
     weight_pts = load_csv.weightTeamSize
     group_size = [0] * len(load_csv.projectIDs)
 
-    # print(group_size)
-
     def group(group_size):
         for i in range(len(load_csv.studentID)):
             group_size[inputArray[i]] = group_size[inputArray[i]] + 1
         return group_size
 
-    group(group_size)
-    # print(group(group_size))
-
     for i in range(len(group_size)):
         if group_size[i] > 0:
-            # print('mininum team size:', load_csv.minTeamSize[i])
-            # print('maximum team size:', load_csv.maxTeamSize[i])
-            # print('this group has', group_size[i], 'people')
-
             if load_csv.minTeamSize[i] <= group_size[i] and group_size[i] <= load_csv.maxTeamSize[i]:
                 totalPTS += weight_pts
-                # print('total PTS score =', totalPTS)
             else:
-                # print('No bonus!')
                 pass
 
     return totalPTS
-
 
 # This calculates penalty for violating studentAvoid constraint
 def pointsAvoid(inputArray):
     totalPSA = 0
     weight_psa = load_csv.weightAvoid
-    # clear = [0] * len(load_csv.studentID)
-    # bad = [0] * len(load_csv.studentID)
-    # penalty = 0
-
-    # print(load_csv.studentID)
-    # for i in load_csv.studentID:
-    #    print(load_csv.studentID[0])
-
-    # print(load_csv.studentAvoidN)
-    # for i in load_csv.studentAvoidN:
-    #    print(load_csv.studentAvoidN[0])
-
-    clear = 0
     bad = 0
 
-    # loop for checking for matches (violations) for each group
-    # print('==============================================')
     for j in range(len(load_csv.projectIDs)):
-        # print('Group ID: ',load_csv.projectIDs[j])
         for i in range(len(inputArray)):
             if range(inputArray[i] == j):
-                # print('group: ', load_csv.projectIDs, 'studentID: ', load_csv.studentID[i], 'avoid: ', load_csv.studentAvoidN[i])
                 if (i in load_csv.studentID) == (
                         i in load_csv.studentAvoidN):  # (i in inputArray) == (i in load_csv.studentID):
-                    # print(load_csv.studentID)
                     # print('match detected')
                     bad += 1
 
-                else:
-                    # print(load_csv.studentID[i])
-                    # print(load_csv.studentAvoidN[i])
-                    # print('no match')
-                    clear += 1
-        # print('==============================================')
-
-    # counts how many groups violoated
-    # for i in range(len(inputArray)):
-    #        if bad[i] > 0:
-    #            print('match detected in group: ', inputArray[i])
-    #            penalty += 1
-
-    # WeightAvoid multiplied by matches found
-    # print('Match found: ', bad)
-    # print('No match found: ', clear)
     totalPSA -= (weight_psa * bad)
     # print('PSA score in loop',totalPSA)
     # print('Current score: ', totalPSA)
 
     return totalPSA
-
 
 def scoringMode(inputArray):
 
