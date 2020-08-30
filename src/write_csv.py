3e0bc8fa96f2149817ed61e64dd5d60aafbcfef1
Bliss <brass@pdx.edu>
2020-08-29 23:54:45 -0700

    Fixing write_csv PROGRAM READY (for testing)

--- src/write_csv.py
+++ src/write_csv.py
@@ -2,19 +2,20 @@
 import pandas as pd
 import prints
 
-
 # insert at line 102 in main.py
 
 def outputCreator(studentFileData, outputFile, optimalSolution):
     df = studentFileData
-    # prints.debug(f"{df}") # Before changes output
-    # Copy the first file so we don't overwrite it
-    df.to_csv(studentFileData, index=False)  # Setting index to False prevents storing of the row numbers in the csv
+    prints.debug(f"{df}") # Before changes output
+
+    # Turning optimalSolution back into a series of ints
+    optimalSolution.tolist()
+    optimalSolution = list(map(int, optimalSolution)) 
 
     df['assignment'] = ''  # If there's no 'Assignment' column, create its header
     new_data = pd.DataFrame({'assignment': optimalSolution})  # Assign data to that header
     df.update(new_data)  # Adding them to the csv
 
     # Writes/Overwrites to a csv named outputFile.csv
-    df.to_csv(outputFile, index=False)
-    # prints.debug(f"{df}") # After changes output
+    df.to_csv(outputFile, index=False, float_format='%g') # float_format g keeps panda's from generating excessively long decimals
+    prints.debug(f"{df}") # After changes output
