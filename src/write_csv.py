5b7e534cd860806fefbc20475edc45d34653b980
Bliss <brass@pdx.edu>
2020-08-31 14:03:50 -0700

    Fixing write_csv.py

--- src/write_csv.py
+++ src/write_csv.py
@@ -1,21 +1,22 @@
 # Creates .csv files with the assignment column added in and filled.
 import pandas as pd
 import prints
+import load_csv
 
 # insert at line 102 in main.py
 
 def outputCreator(studentFileData, outputFile, optimalSolution):
     df = studentFileData
-    prints.debug(f"{df}") # Before changes output
 
-    # Turning optimalSolution back into a series of ints
-    optimalSolution.tolist()
-    optimalSolution = list(map(int, optimalSolution)) 
+    # Matching projectID's to their indexed number in optimalSolution
+    for i in range(len(optimalSolution)):
+        optimalSolution[i] = load_csv.projectIDs[int(optimalSolution[i])]
 
     df['assignment'] = ''  # If there's no 'Assignment' column, create its header
     new_data = pd.DataFrame({'assignment': optimalSolution})  # Assign data to that header
     df.update(new_data)  # Adding them to the csv
+    df = df.astype({"assignment": int}) # Treats assignment outputs as ints
 
+    prints.debug(f"{df} After") # After changes output
     # Writes/Overwrites to a csv named outputFile.csv
-    df.to_csv(outputFile, index=False, float_format='%g') # float_format g keeps panda's from generating excessively long decimals
-    prints.debug(f"{df}") # After changes output
+    df.to_csv(outputFile, index=False)
